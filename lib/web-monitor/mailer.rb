ca4862135855abff9c4822f39941311f60914961
Adnan Mohd Shukor <adnan.shukor@gmail.com>
2016-06-14 15:00:45 +0800

    adding gmail service as email sender
    adding gmail service as email sender

--- lib/web-monitor/mailer.rb
+++ lib/web-monitor/mailer.rb
@@ -2,11 +2,28 @@ module WebMonitor
   class Mailer
     def initialize(config)
       @email = config.alert_mail
+      @gmail = config.use_gmail
+      @gmailuser = config.gmail_username
+      @gmailpass = config.gmail_password
     end
 
     def send(msg)
       return unless @email
-      system %(echo '' | mail -s "#{msg}" #{@email})
+      if @gmail
+        require 'gmail'
+
+        gmail = Gmail.new(@gmailuser, @gmailpass)
+        gmail.deliver do
+          to @email
+          subject "#{msg}"
+          text_part do
+            body "#{msg}"
+          end
+        end
+        gmail.deliver(email)
+      else
+        system %(echo '' | mail -s "#{msg}" #{@email})
+      end
     end
   end
 end
